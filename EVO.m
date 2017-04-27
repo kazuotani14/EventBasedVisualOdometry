f78f614a84f0180f7659f310aeb8e861c40e6b65
Status Kuo <kuobenj@gmail.com>
2017-04-27 18:25:34 -0400

    making some changes to function prototypes, adding a distortion correcting file

--- EVO.m
+++ EVO.m
@@ -9,10 +9,16 @@ load shapes_translation_groundtruth.mat
 % I'm actually not sure what to do here because our groundtruth 
 % trajectory times are not aligned with event images.
 start_time = 0;
-[first_image, end_time] = GetEventImage(start_time, event_mat);
-while end_time < kf_times(1)
+end_time = groundtruth_mat(1,1);
+[first_image, end_time] = GetEventImage(start_time, end_time, event_mat);
+
+[H_planes, H_tfs] = DiscretizeKeyframe(first_image, groundtruth_mat(1,2:end));
+
+groundtruth_idx = 2;
+while end_time < 1.3
 	% DiscretizeKeyframe()
-	[new_image, end_time] = GetEventImage(end_time, event_mat);
+	[new_image, end_time] = GetEventImage(end_time, groundtruth_mat(groundtruth_idx,1), event_mat);
 	% figure;
-	% imshow(new_image);
+	imshow(new_image);
+	groundtruth_idx = groundtruth_idx + 1;
 end
---
+++ GetNewMapPoints.m
@@ -0,0 +1,11 @@
+%This function will look for high density places in the H_planes, 
+% and return these points in 3D for addition to the map
+
+function [new_map_points] = GetNewMapPoints(H_planes, kf_pose)
+	new_map_points = [];
+end
+
+% FindClusters is a helper to find clusters on each plane
+function [found_cluster_image] = FindClusters(H_plane)
+	found_cluster_image = [];
+end
