78fd3e452b2041981cb56f55278468eeb349dee0
Cyrus Xiyuan Liu <Cyrus@Cyrus-MBP.wv.cc.cmu.edu>
2017-04-29 17:39:45 -0400

    minor fix

--- FindPoseToKfH.m
+++ FindPoseToKfH.m
@@ -15,14 +15,14 @@ function [H_kis] = FindPoseToKfH(kf_pose, i_pose, calib)
     kf_R = custom_quat2rotm(kf_quat);
     % kf_R = quat2rotm(kf_quat);
 %     kf_H = K'*[kf_R, kf_T];
-    kf_H = K*[kf_R(1:2,:), kf_T];
+    kf_H = K'*[kf_R(:,1:2), kf_T];
     
     i_T = i_pose(2:4)';
     i_quat = i_pose(5:8);
     i_R = custom_quat2rotm(i_quat);
     % i_R = quat2rotm(i_quat);
 %     i_H = K'*[i_R, i_T];
-    i_H = K*[i_R(1:2,:), i_T];
+    i_H = K'*[i_R(:,1:2), i_T];
     
     H_kis = kf_H/i_H;
 end
