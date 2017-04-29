de19f10a76c515ca725ff550aa35663c4eb66a0d
Aum Jadhav <aum.jadhav@gmail.com>
2017-04-29 02:35:20 -0400

    Update GetClusters.m

--- GetClusters.m
+++ GetClusters.m
@@ -10,6 +10,6 @@ filter = fspecial('gaussian', filt_size); % Sigma to be determined
 [confidence_map, pix_depth] = max(KF_DSI,[],3);
 
 threshold_map = confidence_map > (imfilter(confidence_map, filter) - C);
-depth_map = pix_depth & threshold_map; % Zero depth for pixels with low confidence
+depth_map = pix_depth .* threshold_map; % Zero depth for pixels with low confidence
 
 end
