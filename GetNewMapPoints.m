8598ed4595cf56239b9be657860d4fcbb3ce3950
Status Kuo <kuobenj@gmail.com>
2017-04-30 20:48:21 -0400

    further GetNewMapPoints changes

--- GetNewMapPoints.m
+++ GetNewMapPoints.m
@@ -16,13 +16,16 @@ function [new_map_points] = GetNewMapPoints(depth_map, kf_pose, KF_scaling, KF_d
     kf_R = CustomQuat2RotM(kf_quat);
 
 	tform = [kf_R, kf_T;
-			zeros(1,3), 1];
+			zeros(1,3), 1]; %world to kf tform
 
 	valid_idx = find(depth_map);
 
 	[valid_y, valid_x] = ind2sub(size(depth_map), valid_idx);
 
-	points_in_camera_frame = [KF_scaling(depth_map(valid_y,valid_x),1).*valid_x, KF_scaling(depth_map(valid_y,valid_x),2).*valid_y, KF_depths(depth_map(valid_y,valid_x))];
+    c_x_mat = repmat(size(depth_map,2)/2, size(valid_x,1), 1);
+    c_y_mat = repmat(size(depth_map,1)/2, size(valid_y,1), 1);
+
+	points_in_camera_frame = [KF_scaling(depth_map(valid_idx),1).*(valid_x-c_x_mat), KF_scaling(depth_map(valid_idx),2).*(valid_y-c_y_mat), KF_depths(depth_map(valid_idx))'];
 
 	new_map_points = [points_in_camera_frame, ones(size(points_in_camera_frame,1),1)]*tform';
     new_map_points = new_map_points(:,1:3);
